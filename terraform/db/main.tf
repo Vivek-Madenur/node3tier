275264657f181e3ca04dec87b53623b41ae576e8
Vivek <vivek.madenur@gmail.com>
2022-07-04 10:05:13 +0530

    pipeline

--- terraform/db/main.tf
+++ terraform/db/main.tf
@@ -32,7 +32,7 @@ provider "google" {
 
 # [START cloud_sql_postgres_instance_80_db_n1_s2]
 resource "google_sql_database_instance" "instance" {
-  name             = "postgres-instance7"
+  name             = "postgres-instance123"
   region           = "us-central1"
   database_version = "POSTGRES_14"
 
